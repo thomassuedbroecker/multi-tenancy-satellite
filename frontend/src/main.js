0bd658edbd5a35f2ae006f33dd9ebeb488f11273
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2021-08-25 14:29:59 +0200

    add authentication to main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,7 +8,7 @@ import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 import store from './store'
 import router from './router';
-
+import AppID from 'ibmcloud-appid-js';
 
 
 //import { MwcTopAppBarFixed } from 'vue-material/dist/components'
@@ -20,11 +20,72 @@ import router from './router';
 
 Vue.use(VueMaterial)
 
-new Vue({
-  store,
-  router,
-  render: h => h(App)
-}).$mount('#app')
+/**********************************/
+/* Set variable for authentication
+/**********************************/
+let appid_init;
+let user_info;
 
+/**********************************/
+/* App ID authentication init
+/**********************************/
+
+appid_init = {
+  //web-app-tenant-a-single
+  appid_clientId: window.VUE_APPID_CLIENT_ID,
+  appid_discoveryEndpoint: window.VUE_APPID_DISCOVERYENDPOINT
+}
+
+store.commit("setAppID", appid_init);
+
+let initOptions = {
+  clientId: store.state.appid_init.appid_clientId , discoveryEndpoint: store.state.appid_init.appid_discoveryEndpoint
+}
+
+/**********************************/
+/* Functions 
+/**********************************/
+async function asyncAppIDInit(appID) {
+
+  var appID_init_Result = await appID.init(initOptions);
+  console.log("--> log: appID_init_Result ", appID_init_Result);
+  
+  try {
+    /******************************/
+    /* Authentication
+    /******************************/
+    let tokens = await appID.signin();
+    console.log("--> log: tokens ", tokens);   
+    user_info = {
+      isAuthenticated: true,
+      idToken : tokens.idToken,
+      accessToken: tokens.accessToken,
+      name : tokens.idTokenPayload.name
+    }
+    store.commit("login", user_info);
+    return true;
+  } catch (e) {
+    console.log("--> log: error ", e);
+    return false;
+  } 
+};
+
+/**********************************/
+/* Create vue appication instance
+/**********************************/
+let appID = new AppID();
+let init_messsage = "";
+if (!(init_messsage=asyncAppIDInit(appID))) {
+  console.log("--> log: init_messsage : " + init_messsage);
+  window.location.reload();
+} else {
+    console.log("--> log: init_messsage : " + init_messsage);
+    // Vue application instance
+    new Vue({
+      store,
+      router,
+      render: h => h(App)
+    }).$mount('#app')
+}
 
 export { default as Messaging } from "./messaging.js";
