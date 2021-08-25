9a638ca6f98c04ed30d25efbb43d434ad6bb3c2a
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2021-08-25 14:17:07 +0200

    configure vue-store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,9 +12,35 @@ export default new Vuex.Store({
     authenticationEnabled: "authentication-enabled-no",
     products: [],
     categories: [],
-    commands: []
+    commands: [],
+    appid_init: {
+      appid_clientId: " ",
+      appid_discoveryEndpoint: " "
+    },
+    user: {
+      isAuthenticated: false,
+      name: "",
+      idToken: "",
+      accessToken: ""
+    },
   },
   mutations: {
+    setAppID(state, payload) {
+      state.appid_init.appid_clientId = payload.appid_clientId;
+      state.appid_init.appid_discoveryEndpoint = payload.appid_discoveryEndpoint;
+    },
+    logout(state) {
+      state.user.isAuthenticated = false;
+      state.user.name = "";
+      state.user.idToken = "";
+      state.user.accessToken = "";
+    },
+    login(state, payload) {
+      state.user.isAuthenticated = true;
+      state.user.idToken = payload.idToken;
+      state.user.accessToken = payload.accessToken;
+      state.user.name = payload.name;
+    },
     sendCommand(state, payload) {
       payload.status = COMMAND_STATUS_INVOKED;
       state.commands.push(payload)
